badd419ac9dc690cad615496e26e9dafa632da54
KarlinGreen <kcgreen@rogers.com>
2016-02-13 23:00:11 -0500

    Update 1
    Created some new pages, and a master header to easily add/edit the
    navigation for each page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Portfolio' });
+});
+
+router.get('/aboutMe', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contactMe', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/myProjects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+router.get('/myServices', function(req, res, next) {
+  res.render('index', { title: 'Services' });
 });
 
 module.exports = router;
