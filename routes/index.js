64cd2adeb2122d656a3fbc335e6b81dc343c41db
KarlinGreen <kcgreen@rogers.com>
2016-02-20 17:58:38 -0500

    Update 7
    Commenting added and some bootstrap styles.

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* get the home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Portfolio' });
 });
 
+/* get the aboutMe page */
 router.get('/aboutMe', function(req, res, next) {
   res.render('aboutMe', { title: 'About' });
 });
 
+/* get the contactMe page */
 router.get('/contactMe', function(req, res, next) {
   res.render('contactMe', { title: 'Contact' });
 });
 
+/* get the myProjects page */
 router.get('/myProjects', function(req, res, next) {
   res.render('myProjects', { title: 'Projects' });
 });
 
+/* get the myServices page */
 router.get('/myServices', function(req, res, next) {
   res.render('myServices', { title: 'Services' });
 });
